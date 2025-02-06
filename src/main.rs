be79077138228257e85db3c5e91f0f7b0b8ea1cf
Jooonnnaass <produktiverjonas@gmail.com>
2025-02-06 10:36:10 +0100

    add axum and index route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(index));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
+
+async fn index() -> impl IntoResponse {
+    let index_template = IndexTemplate {};
+
+    (StatusCode::OK, Html(index_template.render().unwrap())).into_response()
 }
